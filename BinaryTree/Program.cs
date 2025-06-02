f2ebcce8d2699d19aa635345285cde5402ca551d
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-06-02 20:23:20 +0700

    BinaryTree IN PROCESS

---
+++ BinaryTree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите размер дерева: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			Random rnd = new Random(0);
+			Tree tree = new Tree();
+			for(int i = 0; i < n; i++)
+			{
+				tree.Insert(rnd.Next(100),tree.Root);
+			}
+			tree.Print(tree.Root);
+			Console.WriteLine();
+			Console.WriteLine($"Min value in tree: {tree.MinValue(tree.Root)}");
+			Console.WriteLine($"Max value in tree: {tree.MaxValue(tree.Root)}");
+			Console.WriteLine($"Кол-во элементов дерева: {tree.Count(tree.Root)}");
+			Console.WriteLine($"Сумма элементов дерева: {tree.Sum(tree.Root)}");
+
+			UniqueTree u_tree = new UniqueTree();
+			for(int i = 0; i < n; i++)
+			{
+				u_tree.Insert(rnd.Next(100),u_tree.Root);
+			}
+			tree.Print(u_tree.Root);
+			Console.WriteLine();
+			Console.WriteLine($"Min value in tree: {u_tree.MinValue(u_tree.Root)}");
+			Console.WriteLine($"Max value in tree: {u_tree.MaxValue(u_tree.Root)}");
+			Console.WriteLine($"Кол-во элементов дерева: {u_tree.Count(u_tree.Root)}");
+			Console.WriteLine($"Сумма элементов дерева: {u_tree.Sum(u_tree.Root)}");
+		}
+	}
+}
